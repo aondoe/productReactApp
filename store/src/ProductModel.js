52b568a02d0a4eda0afb30b5ef9c4818697de6cd
Aondongu Tivzenda <aondoe@gmail.com>
2020-05-23 19:37:14 -0400

    Changed the component structure from functional to a Class ProductModel

--- store/src/ProductModel.js
+++ store/src/ProductModel.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import productLine from "./Product_List";   //This is the raw data from an array of object literals
-import ProductFormat from "./ProductFormat"
 import './store.css';
+import ProductFormat from "./ProductFormat";
+import productLine from "./Product_List"
 
-let hello="Welcome to our greatest Store";
 document.title="Super Store";
 
-let productComponent=productLine//.filter(products=>products.price<14)  //Filter out the records by price
-                .sort((a, b) => a.price > b.price ? - 1 : Number(a.price > b.price))  //Sort the records by price
-                .map(products => <ProductFormat key={products.id}
-                        id={products.id}
-                        product_name={products.product_name}
-                        price={products.price}
-                        description={products.description}
-                        />)
-
-function ProductModel(){
-
-return(
-    <div>
-        <h1 class="text-center">{hello}</h1>
-                {productComponent}
-    </div>)}
-
+class ProductModel extends React.Component{
+    
+    render(){
+        let welcome="Welcome to our greatest Store";
+        let productComponent=productLine//.filter(products=>products.price<14)  //Filter out the records by price
+                        .sort((a, b) => a.price > b.price ? - 1 : Number(a.price > b.price)) //Sort the records by price
+                        .map(products => <ProductFormat key={products.id}
+                                id={products.id}
+                                product_name={products.product_name}
+                                price={products.price}
+                                description={products.description}
+                                />)
+    return(
+        <div>
+            <h1 class="text-center">{welcome}</h1>
+                    {productComponent}
+        </div>)
+    }
+}
 export default ProductModel;
